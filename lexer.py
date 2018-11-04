01124869f88079fe18cc1f5b75a33e5d3e720e3b
Angel Hernandez <ahernandez0691@gmail.com>
2018-11-04 11:57:03 -0800

    integers > 9 supported + whitespace is now ignored

--- lexer.py
+++ lexer.py
@@ -2,7 +2,9 @@
 INTEGER = 'INTEGER'
 PLUS = 'PLUS'
 EOF = 'EOF'
-WHITESPACE = 'WHITESPACE'
+MINUS = 'MINUS'
+
+
 class ParseException(Exception): pass
 
 class Token:
@@ -19,7 +21,7 @@ class Token:
 			Token(INTEGER, 3)
 			Token(PLUS, '+')
 		"""
-		return 'Token({}, {})'.format(self.type, self.value)
+		return 'Token({}, {})'.format(self.type, repr(self.value))
 
 	def __repr__(self):
 		return self.__str__()
@@ -32,9 +34,31 @@ class Interpreter:
 		self._pos = 0
 		# current token instance
 		self._current_token = None
+		self._current_char = self._text[self._pos]
 
 	def error(self):
 		raise ParseException('Error parsing input.')
+
+	def advance(self):
+		"""Advance the 'pos' pointer and set the 'current_char' variable."""
+		self._pos += 1
+		if self._pos > len(self._text) - 1:
+			self._current_char = None #End of Input
+		else:
+			self._current_char = self.text[self._pos]
+
+	def skip_whitespace(self):
+		"""Advance the pointer and ignore whitespace characters"""
+		while self._current_char is not None and self._current_char.isspace():
+			self.advance()
+
+	def integer(self):
+		"""Return an integer > 9 consumed from the input"""
+		result = ''
+		while self._current_char is not None and self._current_char.isdigit():
+			result += self._current_char
+			self.advance
+		return int(result)
 
 	def get_next_token(self):
 		"""Lexical analyzer (scanner / tokenizer)
@@ -42,35 +66,29 @@ class Interpreter:
 		This method is responsible for breaking a sentence
 		apart into tokens. One token at a time
 		"""
-		text = self._text
+		while self._current_char is not None:
+			
+			if self._current_char.isspace():
+				self.skip_whitespace()
+				continue
 
-		if self._pos > len(text) - 1:
-			return Token(EOF, None)
+			if self._current_char.isdigit():
+				return Token(INTEGER, self.integer())
 
-		current_char = text[self._pos]
+			if self._current_char == '+':
+				self.advance()
+				return Token(PLUS, '+')
 
-		if current_char.isdigit():
-			token = Token(INTEGER, int(current_char))
-			self._pos += 1
-			return token
+			if self._current_char == '-':
+				self.advance()
+				return Token(MINUS, '-')
 
-		if current_char == '+':
-			token = Token(PLUS, current_char)
-			self._pos += 1
-			return token
+			self.error()
 
-		# Account for whitespace
-		if current_char in ['\t', ' ']:
-			token = Token(WHITESPACE, None)
-			self._pos += 1
-			return token
-
-
-
-		self.error()
+		return Token(EOF, None)
 
 	def eat(self, token_type):
-		print(self._current_token.type + '====' + token_type)
+
 		if self._current_token.type == token_type:
 			self._current_token = self.get_next_token()
 		else:
@@ -79,16 +97,28 @@ class Interpreter:
 	def expr(self):
 		self._current_token = self.get_next_token()
 
+
 		left = self._current_token
 		self.eat(INTEGER)
 
 		op = self._current_token
-		self.eat(PLUS)
+		if op.type == PLUS:
+			self.eat(PLUS)
+		elif op.type == MINUS:
+			self.eat(MINUS)
+		else:
+			self.error()
 
 		right = self._current_token
 		self.eat(INTEGER)
 
-		result = eval('{}{}{}'.format(left.value, op.value, right.value))
+		if op.type == PLUS:
+			result = left.value + right.value
+		elif op.type == MINUS:
+			result = left.value - right.value
+		else:
+			self.error()
+
 		return result
 
 def main():
